4698fb3ba5f37c61566bd2bd5b76469c0556392f
dipak <deepakmagdum769@gmail.com>
2026-04-13 23:23:09 +0530

    add VariableDemo.java

---
+++ Basics/VariableDemo.java
@@ -0,0 +1,12 @@
+public class VariableDemo{
+    public static void main(String [] args){
+        
+        String name = "Dipak"; // here name = variableName and Dipak is a value
+
+        int age = 23;
+
+        System.out.println("Name is :" + name);
+        System.out.println("Age is :" +age);
+
+    }
+}
