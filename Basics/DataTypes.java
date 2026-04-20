b760fddb569c557b698940eaf829f216351ec5c0
dipak <deepakmagdum769@gmail.com>
2026-04-20 20:03:17 +0530

    feat: add DataTypes in java

---
+++ Basics/DataTypes.java
@@ -0,0 +1,12 @@
+public class DataTypes {
+    public static void main(String[]args){
+        // In java their are two types of categoris
+        // 1. primitive 
+
+        int num = 5;
+        System.out.println("The number is :" +num);
+
+
+    }
+    
+}
