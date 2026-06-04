63b9668f19ee7cef502a84a9ab3c00b8d924e889
dipak <deepakmagdum769@gmail.com>
2026-06-04 10:58:21 +0530

    feat: add primitive and non-primitive data types example

---
+++ 01-Basics/PrimitiveVsNonPrimitive.java
@@ -0,0 +1,24 @@
+public class PrimitiveVsNonPrimitive {
+
+    
+     public static void main(String[] args) {
+
+        // Primitive Data Types
+        int number = 10;
+        double price = 99.99;
+        char grade = 'A';
+        boolean isJavaEasy = true;
+
+        // Non-Primitive Data Types
+        String name = "Dipak";
+        int[] marks = {85, 90, 95};
+
+        System.out.println(number);
+        System.out.println(price);
+        System.out.println(grade);
+        System.out.println(isJavaEasy);
+
+        System.out.println(name);
+        System.out.println(marks[0]);
+    }
+}
