58201f56818b406662ebfeaa8b2cbc47537e8174
dipak <deepakmagdum769@gmail.com>
2026-06-04 10:49:38 +0530

    feat: add comments demo example

---
+++ 01-Basics/CommentsDemo.java
@@ -0,0 +1,15 @@
+public class CommentsDemo {
+
+    public static void main(String[] args) {
+
+        // This is a single line comment
+        System.out.println("Java Comments");
+
+        /*
+           This is a
+           multi-line comment
+        */
+
+        System.out.println("Learning Java");
+    }
+}
