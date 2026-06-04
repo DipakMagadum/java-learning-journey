afa7bc81b2b247949cff6aec6697be68a14ea2ee
dipak <deepakmagdum769@gmail.com>
2026-06-04 10:43:59 +0530

    feat: add escape sequence examples

---
+++ 01-Basics/EscapeSequence.java
@@ -0,0 +1,11 @@
+public class EscapeSequence {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello\nJava");
+
+        System.out.println("Name\tAge");
+
+        System.out.println("\"Java Programming\"");
+    }
+}
