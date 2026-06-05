b9bc5ac942d4144c98af6124e0ce32759bb54131
dipak <deepakmagdum769@gmail.com>
2026-06-05 12:57:15 +0530

    feat: add increment and decrement example

---
+++ 01-Basics/IncrementDecrement.java
@@ -0,0 +1,15 @@
+public class IncrementDecrement {
+
+    public static void main(String[] args) {
+
+        int number = 10;
+
+        System.out.println(number);
+
+        number++;
+        System.out.println(number);
+
+        number--;
+        System.out.println(number);
+    }
+}
