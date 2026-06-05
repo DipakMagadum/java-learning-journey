6ba7932fe9650e12e350f1593cfa941e58b1044d
dipak <deepakmagdum769@gmail.com>
2026-06-05 12:59:14 +0530

    feat: add print and println example

---
+++ 01-Basics/PrintVsPrintln.java
@@ -0,0 +1,13 @@
+public class PrintVsPrintln {
+
+    public static void main(String[] args) {
+
+        System.out.print("Hello ");
+        System.out.print("Java");
+
+        System.out.println();
+
+        System.out.println("Hello");
+        System.out.println("Java");
+    }
+}
