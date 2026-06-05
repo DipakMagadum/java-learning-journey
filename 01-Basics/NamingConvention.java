c88fa59517460ec3e9a653008223db2500b60d90
dipak <deepakmagdum769@gmail.com>
2026-06-05 12:53:49 +0530

    feat: add naming convention example

---
+++ 01-Basics/NamingConvention.java
@@ -0,0 +1,16 @@
+public class NamingConvention {
+
+    public static void main(String[] args) {
+
+        // Class Name -> PascalCase
+        // Variable Name -> camelCase
+        // Constant Name -> UPPER_CASE
+
+        String studentName = "Dipak";
+
+        final double PI_VALUE = 3.14;
+
+        System.out.println(studentName);
+        System.out.println(PI_VALUE);
+    }
+}
