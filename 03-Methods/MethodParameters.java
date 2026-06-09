6dd3c17a89c8c15529ca0915085f80de956a090f
dipak <deepakmagdum769@gmail.com>
2026-06-09 20:08:32 +0530

    feat: add method parameters example

---
+++ 03-Methods/MethodParameters.java
@@ -0,0 +1,12 @@
+public class MethodParameters {
+
+    public static void printMessage(String name) {
+
+        System.out.println("Welcome " + name);
+    }
+
+    public static void main(String[] args) {
+
+        printMessage("Dipak");
+    }
+}
