fbc4c4e8e6ab19994a7ce76aa041f588a706592e
dipak <deepakmagdum769@gmail.com>
2026-06-09 20:09:38 +0530

    feat: create simple greeting method example

---
+++ 03-Methods/GreetingMethod.java
@@ -0,0 +1,13 @@
+public class GreetingMethod {
+
+    public static void greetUser() {
+
+        System.out.println("Good Morning");
+        System.out.println("Welcome to Java");
+    }
+
+    public static void main(String[] args) {
+
+        greetUser();
+    }
+}
