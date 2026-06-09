77e63bd4b1c9c2a479e19c996b98ecf047fb22fa
dipak <deepakmagdum769@gmail.com>
2026-06-09 20:03:42 +0530

    feat: introduce basic method creation and calling

---
+++ 03-Methods/MethodDemo.java
@@ -0,0 +1,11 @@
+public class MethodDemo {
+
+    public static void greet() {
+        System.out.println("Welcome to Java Methods");
+    }
+
+    public static void main(String[] args) {
+
+        greet();
+    }
+}
