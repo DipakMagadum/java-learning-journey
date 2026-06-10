d7b594233f203307eb433985a766eff6e667c876
dipak <deepakmagdum769@gmail.com>
2026-06-10 19:52:18 +0530

    feat: add square calculation using method

---
+++ 03-Methods/SquareMethod.java
@@ -0,0 +1,14 @@
+public class SquareMethod {
+
+    public static int findSquare(int number) {
+
+        return number * number;
+    }
+
+    public static void main(String[] args) {
+
+        int result = findSquare(6);
+
+        System.out.println("Square: " + result);
+    }
+}
