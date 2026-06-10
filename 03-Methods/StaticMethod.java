0f69512ec2c724b1ea2fef6a108ff3fcb85a9d4e
dipak <deepakmagdum769@gmail.com>
2026-06-10 19:48:47 +0530

    feat: add static method example

---
+++ 03-Methods/StaticMethod.java
@@ -0,0 +1,12 @@
+public class StaticMethod {
+
+    public static void showMessage() {
+
+        System.out.println("This is a static method");
+    }
+
+    public static void main(String[] args) {
+
+        showMessage();
+    }
+}
