159c380ca154e4b4632ef0f54f01aab5ea3701f3
dipak <deepakmagdum769@gmail.com>
2026-06-11 10:15:21 +0530

    feat: solve factorial using recursion

---
+++ 03-Methods/RecursiveFactorial.java
@@ -0,0 +1,18 @@
+public class RecursiveFactorial {
+
+    public static int factorial(int number) {
+
+        if (number == 0 || number == 1) {
+            return 1;
+        }
+
+        return number * factorial(number - 1);
+    }
+
+    public static void main(String[] args) {
+
+        int result = factorial(5);
+
+        System.out.println("Factorial: " + result);
+    }
+}
