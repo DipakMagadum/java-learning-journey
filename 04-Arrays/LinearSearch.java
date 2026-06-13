02f24bff1cfeaa1bfa63b0f0abdb036e7cc32197
dipak <deepakmagdum769@gmail.com>
2026-06-13 10:06:14 +0530

    feat: implement linear search on integer array

---
+++ 04-Arrays/LinearSearch.java
@@ -0,0 +1,24 @@
+public class LinearSearch {
+
+    public static void main(String[] args) {
+
+        int[] numbers = {10, 20, 30, 40, 50};
+        int target = 30;
+
+        boolean found = false;
+
+        for (int i = 0; i < numbers.length; i++) {
+
+            if (numbers[i] == target) {
+                found = true;
+                break;
+            }
+        }
+
+        if (found) {
+            System.out.println("Element Found");
+        } else {
+            System.out.println("Element Not Found");
+        }
+    }
+}
