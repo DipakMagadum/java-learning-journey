b90c1494d37def5b3035a083be0a0e08fd8b17ab
dipak <deepakmagdum769@gmail.com>
2026-06-13 10:05:17 +0530

    feat: identify smallest value in array

---
+++ 04-Arrays/FindMinimum.java
@@ -0,0 +1,18 @@
+public class FindMinimum {
+
+    public static void main(String[] args) {
+
+        int[] numbers = {25, 10, 45, 18, 32};
+
+        int min = numbers[0];
+
+        for (int i = 1; i < numbers.length; i++) {
+
+            if (numbers[i] < min) {
+                min = numbers[i];
+            }
+        }
+
+        System.out.println("Minimum Number: " + min);
+    }
+}
