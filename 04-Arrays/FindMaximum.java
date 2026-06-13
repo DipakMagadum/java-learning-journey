8d86e14fb87110910ac99a70408b380db7bf855b
dipak <deepakmagdum769@gmail.com>
2026-06-13 10:04:33 +0530

    feat: find largest element in an array

---
+++ 04-Arrays/FindMaximum.java
@@ -0,0 +1,18 @@
+public class FindMaximum {
+
+    public static void main(String[] args) {
+
+        int[] numbers = {25, 10, 45, 18, 32};
+
+        int max = numbers[0];
+
+        for (int i = 1; i < numbers.length; i++) {
+
+            if (numbers[i] > max) {
+                max = numbers[i];
+            }
+        }
+
+        System.out.println("Maximum Number: " + max);
+    }
+}
