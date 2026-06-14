d99469155bd04a21cde10cbf8325517735a6ed98
dipak <deepakmagdum769@gmail.com>
2026-06-14 11:02:02 +0530

    feat: calculate length of a string

---
+++ 05-Strings/StringLength.java
@@ -0,0 +1,11 @@
+public class StringLength {
+
+    public static void main(String[] args) {
+
+        String message = "Java Programming";
+
+        int length = message.length();
+
+        System.out.println("Length: " + length);
+    }
+}
