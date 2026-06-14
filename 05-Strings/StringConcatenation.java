8358d419a55772e70cefb47cdd605d7ea77b5130
dipak <deepakmagdum769@gmail.com>
2026-06-14 11:02:32 +0530

    feat: combine strings using concatenation

---
+++ 05-Strings/StringConcatenation.java
@@ -0,0 +1,12 @@
+public class StringConcatenation {
+
+    public static void main(String[] args) {
+
+        String firstName = "Dipak";
+        String lastName = "Magadum";
+
+        String fullName = firstName + " " + lastName;
+
+        System.out.println(fullName);
+    }
+}
