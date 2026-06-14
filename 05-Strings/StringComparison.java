6c3c0b455bbca23ace5485cdcf22c462460a7176
dipak <deepakmagdum769@gmail.com>
2026-06-14 11:03:36 +0530

    feat: compare two strings using equals method

---
+++ 05-Strings/StringComparison.java
@@ -0,0 +1,14 @@
+public class StringComparison {
+
+    public static void main(String[] args) {
+
+        String first = "Java";
+        String second = "Java";
+
+        if (first.equals(second)) {
+            System.out.println("Strings are equal");
+        } else {
+            System.out.println("Strings are not equal");
+        }
+    }
+}
