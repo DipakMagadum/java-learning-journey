10a52e6d7ca398f71ae6bc6fc4242bb35433463c
dipak <deepakmagdum769@gmail.com>
2026-06-19 13:47:15 +0530

    feat: add linked list example for task management

---
+++ 07-Collections/LinkedListDemo.java
@@ -0,0 +1,15 @@
+import java.util.LinkedList;
+
+public class LinkedListDemo {
+
+    public static void main(String[] args) {
+
+        LinkedList<String> tasks = new LinkedList<>();
+
+        tasks.add("Complete Java Practice");
+        tasks.add("Push Code to GitHub");
+        tasks.add("Learn Collections");
+
+        System.out.println(tasks);
+    }
+}
