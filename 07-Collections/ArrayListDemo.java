ab0304c02d0b8179f39af90daca21ad02ae4a753
dipak <deepakmagdum769@gmail.com>
2026-06-19 13:46:35 +0530

    feat: introduce ArrayList with basic operations

---
+++ 07-Collections/ArrayListDemo.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+
+public class ArrayListDemo {
+
+    public static void main(String[] args) {
+
+        ArrayList<String> fruits = new ArrayList<>();
+
+        fruits.add("Apple");
+        fruits.add("Banana");
+        fruits.add("Mango");
+
+        System.out.println(fruits);
+    }
+}
